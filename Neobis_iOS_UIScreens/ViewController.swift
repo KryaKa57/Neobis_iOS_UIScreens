53c6e0f346928b4bfcbcd9521cb6bef840c1387d
Alisher <kuanyshbekovalisher57@gmail.com>
2023-11-07 02:22:49 +0600

    Initial Commit

---
+++ Neobis_iOS_UIScreens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_UIScreens
+//
+//  Created by Alisher on 07.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
